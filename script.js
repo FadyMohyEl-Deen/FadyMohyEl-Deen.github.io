0c3b2875e14fb2875aae1b611e3c27bd9e513e3e
FadyMohyEl-Deen <fadymohy@zohomail.com>
2024-09-27 17:24:39 +0300

    added js file

---
+++ script.js
@@ -0,0 +1,54 @@
+let firstname = "Fady"
+console.log(firstname);
+firstname = 25
+console.log(firstname);
+firstname = true
+console.log(true);
+
+
+
+let x = 26
+let y = 5
+
+console.log(x + y);
+console.log(x - y);
+console.log(x % y);
+console.log(x / y);
+console.log(x * y);
+
+
+
+
+let markMass = 78
+let markHeight = 169
+let johnMass = 95
+let johnHeight = 188
+
+let markBMI = markMass / (markHeight ** 2)
+let johnBMI = johnMass / (johnHeight ** 2)
+
+console.log("Mark's BMI= " + markBMI);
+console.log("John's BMI= " + johnBMI);
+
+
+if (markBMI > johnBMI) { console.log(`Mark's BMI ${markBMI} is heighr than John's BMI ${johnBMI}`) }
+else { console.log(`John's BMI ${johnBMI} is heigher than Mark's BMI ${markBMI}`) }
+
+
+
+markBMI == johnBMI ? console.log('Yes') : console.log('No');
+markBMI > johnBMI ? console.log('Mark is heigher') : console.log('John is heigher')
+markBMI < 0.002 ? console.log('Mark, your BMI is okay') : console.log("Mark, start working out you fckin pig")
+johnBMI < 1 ? console.log("John, your BMI is okay") : console.log("John, start working out you fckin pig")
+
+
+
+
+// JS IS LINKED TO HTML RIGHT BEFORE THE BODY END TAG
+// Git clone: clones folder from link
+// Git Add . : adds everything
+// Git commit -m "Comment of changes made"
+// Git push: pushes code to Git repo
+// Git status: shows changes in files
+// Git pull: pulls last updated code from githup to VS code
+
